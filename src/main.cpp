d4b4e0c290ae42a260dc57b603d5b0dc97d4cba8
Francois Gaudin <118230+fgaudin@users.noreply.github.com>
2022-08-28 13:55:31 -0700

    refactor button pressed and speed fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,19 @@
 const int dirPin = 2;
 const int stepPin = 3;
 const int releasePin = 4;
+
 const int directionPin = 5;
 const int enablePin = 6;
 const int slowerPin = 7;
 const int fasterPin = 8;
+
 int speed = 200;
-const int maxSpeed = 500;
+const int maxSpeed = 800;
 const int speedIncr = 10;
 
-int directionButtonState;
-int lastDirectionButtonState = HIGH;
-unsigned long lastDirectionDebounceTime = 0;
-
-int enableButtonState;
-int lastEnableButtonState = HIGH;
-unsigned long lastEnableDebounceTime = 0;
-
-int slowerButtonState;
-int lastSlowerButtonState = HIGH;
-unsigned long lastSlowerDebounceTime = 0;
-
-int fasterButtonState;
-int lastFasterButtonState = HIGH;
-unsigned long lastFasterDebounceTime = 0;
+int buttonState[] = {LOW, LOW, LOW, LOW};
+int lastButtonState[] = {HIGH, HIGH, HIGH, HIGH};
+unsigned long lastDebounceTime[] = {0, 0, 0, 0};
 
 unsigned long debounceDelay = 50;
 
@@ -47,132 +37,95 @@ void setup()
   myStepper.setSpeed(0);
 }
 
-void checkDirection()
+boolean buttonPressed(int pin)
 {
-  int direction = digitalRead(directionPin);
+  const int idx = pin - directionPin;
 
-  if (direction != lastDirectionButtonState)
+  int value = digitalRead(pin);
+  bool pressed = false;
+
+  if (value != lastButtonState[idx])
   {
-    lastDirectionDebounceTime = millis();
+    lastDebounceTime[idx] = millis();
   }
 
-  if ((millis() - lastDirectionDebounceTime) > debounceDelay)
+  if ((millis() - lastDebounceTime[idx]) > debounceDelay)
   {
-    if (direction != directionButtonState)
+    if (value != buttonState[idx])
     {
-      directionButtonState = direction;
+      buttonState[idx] = value;
 
-      if (directionButtonState == LOW)
+      if (buttonState[idx] == LOW)
       {
-        Serial.println("direction button pressed");
-        myStepper.setSpeed(-myStepper.speed());
+        pressed = true;
       }
     }
   }
 
-  lastDirectionButtonState = direction;
-}
+  lastButtonState[idx] = value;
 
-void checkEnable()
-{
-  int enable = digitalRead(enablePin);
-
-  if (enable != lastEnableButtonState)
-  {
-    lastEnableDebounceTime = millis();
-  }
-
-  if ((millis() - lastEnableDebounceTime) > debounceDelay)
-  {
-    if (enable != enableButtonState)
-    {
-      enableButtonState = enable;
-
-      if (enableButtonState == LOW)
-      {
-        Serial.println("enable button pressed");
-        if (myStepper.speed() != 0.0)
-        {
-          Serial.println("motor running: stopping");
-          myStepper.setSpeed(0);
-          myStepper.disableOutputs();
-          digitalWrite(releasePin, HIGH);
-        }
-        else
-        {
-          Serial.println("motor stopped: starting");
-          digitalWrite(releasePin, LOW);
-          delay(2);
-          myStepper.enableOutputs();
-          myStepper.setSpeed(speed);
-        }
-      }
-    }
-  }
-
-  lastEnableButtonState = enable;
-}
-
-void checkSlower()
-{
-  int slower = digitalRead(slowerPin);
-
-  if (slower != lastSlowerButtonState)
-  {
-    lastSlowerDebounceTime = millis();
-  }
-
-  if ((millis() - lastSlowerDebounceTime) > debounceDelay)
-  {
-    if (slower != slowerButtonState)
-    {
-      slowerButtonState = slower;
-
-      if (slowerButtonState == LOW)
-      {
-        Serial.println("slower button pressed");
-        speed = max(speed - speedIncr, 0);
-        myStepper.setSpeed(speed);
-      }
-    }
-  }
-
-  lastSlowerButtonState = slower;
-}
-
-void checkFaster()
-{
-  int faster = digitalRead(fasterPin);
-
-  if (faster != lastFasterButtonState)
-  {
-    lastFasterDebounceTime = millis();
-  }
-
-  if ((millis() - lastFasterDebounceTime) > debounceDelay)
-  {
-    if (faster != fasterButtonState)
-    {
-      fasterButtonState = faster;
-
-      if (fasterButtonState == LOW)
-      {
-        Serial.println("faster button pressed");
-        speed = min(speed + speedIncr, maxSpeed);
-        myStepper.setSpeed(speed);
-      }
-    }
-  }
-
-  lastFasterButtonState = faster;
+  return pressed;
 }
 
 void loop()
 {
-  checkDirection();
-  checkEnable();
-  checkSlower();
-  checkFaster();
+  if (buttonPressed(directionPin))
+  {
+    Serial.println("direction button pressed");
+    speed = -speed;
+    myStepper.setSpeed(speed);
+  }
+
+  if (buttonPressed(enablePin))
+  {
+    Serial.println("enable button pressed");
+    if (myStepper.speed() != 0.0)
+    {
+      Serial.println("motor running: stopping");
+      myStepper.setSpeed(0);
+      myStepper.disableOutputs();
+      digitalWrite(releasePin, HIGH);
+    }
+    else
+    {
+      Serial.println("motor stopped: starting");
+      Serial.print("speed: ");
+      Serial.println(myStepper.speed());
+      digitalWrite(releasePin, LOW);
+      delay(2);
+      myStepper.enableOutputs();
+      myStepper.setSpeed(speed);
+    }
+  }
+
+  if (buttonPressed(slowerPin))
+  {
+    Serial.println("slower button pressed");
+    if (myStepper.speed() >= 0)
+    {
+      speed = max(speed - speedIncr, 0);
+    }
+    else
+    {
+      speed = min(speed + speedIncr, 0);
+    }
+
+    myStepper.setSpeed(speed);
+  }
+
+  if (buttonPressed(fasterPin))
+  {
+    Serial.println("faster button pressed");
+    if (myStepper.speed() >= 0)
+    {
+      speed = min(speed + speedIncr, maxSpeed);
+    }
+    else
+    {
+      speed = max(speed - speedIncr, -maxSpeed);
+    }
+    myStepper.setSpeed(speed);
+  }
 
   myStepper.runSpeed();
 }
